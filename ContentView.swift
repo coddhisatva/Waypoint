cf457529575401e038657085e7057a30eccd7c23
skaymore <conoregan151@gmail.com>
2025-08-16 12:52:21 -0400

    Initial (XCode) Project

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Waypoint
+//
+//  Created by Conor Egan on 8/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaypointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointApp.swift
+//  Waypoint
+//
+//  Created by Conor Egan on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WaypointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
